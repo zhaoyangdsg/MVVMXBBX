8943ce5268f0f058cfda025a53ff5c85efac6343
zhaoyangdsg <747032289@qq.com>
2018-05-08 17:54:20 +0800

    init

---
+++ MVVMXBBX/MVVMXBBX/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVVMXBBX
+//
+//  Created by me2 on 2018/5/8.
+//  Copyright © 2018年 me2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MVVMXBBX/MVVMXBBX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVVMXBBX
+//
+//  Created by me2 on 2018/5/8.
+//  Copyright © 2018年 me2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
