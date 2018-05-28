9144e10d46a7976b019ff33dacc280a978558f50
肇扬 <zhaoyang@zhaoyangdemac-mini.local>
2018-05-28 18:04:08 +0800

    delegate

--- MVVMXBBX/MVVMXBBX/Class/Other/Tools/ZYHttpTool.m
+++ MVVMXBBX/MVVMXBBX/Class/Other/Tools/ZYHttpTool.m
@@ -8,7 +8,7 @@
 
 #import "ZYHttpTool.h"
 
-@interface ZYHttpTool()<NSURLSessionDelegate>
+@interface ZYHttpTool()<NSURLSessionDelegate,NSURLSessionTaskDelegate,NSURLSessionDataDelegate>
 @property(nonatomic,strong)NSURLSession * session;
 @property(nonatomic,strong)NSURLSessionConfiguration *configuration;
 @property(nonatomic,strong)NSOperationQueue *operationQueue;
@@ -59,23 +59,131 @@
     
 }
 
-- (void)requestWithMethod:(NSString *)method url:(NSString *)urlStr parameters:(NSDictionary *)params success:(void (^)(id))success failure:(void (^)(NSError *))failure {
+- (void)requestWithMethod:(NSString *)method url:(NSString *)urlStr parameters:(NSDictionary *)params  success:(void (^)(id))success failure:(void (^)(NSError *))failure {
+    
     NSURL * url;
-    
-        
-        if (self.baseUrl) {
-            url = [NSURL URLWithString:urlStr relativeToURL:self.baseUrl];
-        }else {
-            url = [NSURL URLWithString:urlStr];
-        }
-    
+
+    if (self.baseUrl) {
+        url = [NSURL URLWithString:urlStr relativeToURL:self.baseUrl];
+    }else {
+        url = [NSURL URLWithString:urlStr];
+    }
 
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:30];
     request.HTTPMethod = method;
     if ([method isEqualToString:@"POST"]) {
-        
+        NSData *bodyData = [[self queryParameters:params]  dataUsingEncoding:NSUTF8StringEncoding];
+        [request setHTTPBody:bodyData];
+    }else {
+        NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:false];
+        urlComponents.percentEncodedQuery = [self queryParameters:params] ;
+        request.URL = urlComponents.URL;
     }
+    NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            failure(error);
+        }else {
+            
+        }
+    }];
+    
+    [task resume];
 }
 
+///  把所有参数拼接成字符串
+- (NSString *)queryParameters:(NSDictionary *) parameters {
+    
+    
+    NSMutableArray *params = [NSMutableArray array];
+    NSArray<NSString *> *keys = [ parameters.allKeys sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        if ((NSString *)obj1 < (NSString*)obj2 ) {
+            return NSOrderedAscending;
+        }else {
+            return NSOrderedDescending;
+        }
+    }];
+    
+    for (NSString *key in keys) {
+        [params addObjectsFromArray: [self queryComponentsWithKey:key value:parameters[key]]];
+    }
+    
+    NSMutableString *paramStr = [[NSMutableString alloc]init];
+    for (NSDictionary *paramDic in params ) {
+        [paramDic enumerateKeysAndObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(NSString * _Nonnull key, NSString * _Nonnull obj, BOOL * _Nonnull stop) {
+            [paramStr appendString: [NSString stringWithFormat:@"%@=%@",key,obj]];
+            [paramStr appendString:@"&"];
+        }];
+    }
+    NSLog(@"%@",paramStr);
+
+    return paramStr;
+}
+/// 遍历所有参数,遇到字典,数组 递归, 返回所有参数的字典数组
+- (NSArray*)queryComponentsWithKey:(NSString*)key value:(NSObject*)value {
+    NSMutableArray* components = [NSMutableArray array];
+    if ([value isKindOfClass:[NSDictionary class]]) {
+        [components addObjectsFromArray: [self queryComponentsWithKey:key value:value]];
+    }else if ([value isKindOfClass:[NSArray class]]) {
+        for (NSObject *aryValue in (NSArray *)value) {
+            [components addObjectsFromArray:[self queryComponentsWithKey:key value:aryValue] ];
+        }
+    }else {
+        [components addObject:@{[self escape:key]: [self escape: [NSString stringWithFormat:@"%@",value]]}];
+    }
+    
+    return components;
+}
+
+/// 转码
+- (NSString *)escape:(NSString *)str {
+    return  [str stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+}
+
+
+
+
+
+//{
+//    @"",@"",
+//    @"",@2,
+//    @"dics",{
+//       @"key1",@"xx",
+//        @"key2",@3
+//    }
+//    @"ary",[@"",@"",@""]
+//}
+//@"":@"",@"":@2,@"dics[key1]":@"",@"dics[key2]":@3,@"ary[]",
+/**
+public func queryComponents(fromKey key: String, value: Any) -> [(String, String)] {
+    var components: [(String, String)] = []
+    
+    if let dictionary = value as? [String: Any] {
+        for (nestedKey, value) in dictionary {
+            components += queryComponents(fromKey: "\(key)[\(nestedKey)]", value: value)
+        }
+    } else if let array = value as? [Any] {
+        for value in array {
+            components += queryComponents(fromKey: "\(key)[]", value: value)
+        }
+    } else if let value = value as? NSNumber {
+        if value.isBool {
+            components.append((escape(key), escape((value.boolValue ? "1" : "0"))))
+        } else {
+            components.append((escape(key), escape("\(value)")))
+        }
+    } else if let bool = value as? Bool {
+        components.append((escape(key), escape((bool ? "1" : "0"))))
+    } else {
+        components.append((escape(key), escape("\(value)")))
+    }
+    
+    return components
+}
+*/
+
+
+@end
+
+@interface ZYHttpTool(SessionDelegate)
 
 @end
